a5dc3772087c8d0b3ccaa40ddf6ee8b85d7f0dd9
EldiAnugrah <203040031@mail.unpas.ac.id>
2022-10-17 14:25:19 +0700

    Menyelesaikan Video Laravel 3

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about',[
+        "name" => "Eldi Anugrah",
+        "email" => "203040031@mail.unpas.ac.id",
+        "image" => "eldi.jpeg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('post');
 });
