f875ea06abbe1e817bf19a332a25ace394dd3192
EldiAnugrah <203040031@mail.unpas.ac.id>
2022-11-02 15:56:25 +0700

    menyelesaikan video laravel 16

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,1 @@
+<h1>PPPPPPPPP</h1>
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -12,16 +12,40 @@
                     </button>
                 </div>
             @endif
-            <h1 class="h3 mb-3 font-weight-normal text-center">Login Dulu Banh</h1>
-            <form class="form-signin">
-                <label for="inputEmail" class="sr-only">Email address</label>
-                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-                <label for="inputPassword" class="sr-only">Password</label>
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-                <div class="checkbox mb-3">
+
+            @if (session()->has('loginError'))
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    {{ session('loginError') }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Log in</button>
-            </form>
+            @endif
+
+            <main class="form-signin">
+                <h1 class="h3 mb-3 font-weight-normal text-center">Login Dulu Banh</h1>
+                <form action="/login" method="post">
+                    @csrf
+                    <div class="form-floating">
+                        <label for="email">Email address</label>
+                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
+                            id="email" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
+                        @error('email')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+
+                    </div>
+                    <div class="form-floating">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" class="form-control rounded-bottom " id="password"
+                            placeholder="Password" required>
+                        <div class="checkbox mb-3">
+                        </div>
+                        <button class="btn btn-lg btn-primary btn-block" type="submit">Log in</button>
+                </form>
+            </main>
             <small class="d-block text-center mt-4">Belum daftar? <a href="/register">Gas Daftar La</a></small>
         </div>
     </div>
